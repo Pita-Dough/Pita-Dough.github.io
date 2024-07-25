557e3a65df04f933905ab4be609edd7a46398539
Peter Doe <37709416+Pita-Dough@users.noreply.github.com>
2024-07-25 16:02:26 -0700

    Update toggle.js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -21,10 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
             // }
             if (toggleBlocks[index].style.maxHeight){
                 toggleBlocks[index].style.maxHeight = null;
-                button.innerHTML = "ABSTRACT <i class='btn btn-sm z-depth-0' type='button' target='_blank' style='font-size:12px'></i>"; // this − minus sign is of equal length as +  
+                button.innerHTML = "Abstract <i class='btn btn-sm z-depth-0' type='button' target='_blank' style='font-size:12px'></i>"; // this − minus sign is of equal length as +  
               } else {
                 toggleBlocks[index].style.maxHeight = toggleBlocks[index].scrollHeight + "px";
-                button.innerHTML = "ABSTRACT <i class='btn btn-sm z-depth-0' type='button' target='_blank' style='font-size:12px'></i>"; 
+                button.innerHTML = "Abstract <i class='btn btn-sm z-depth-0' type='button' target='_blank' style='font-size:12px'></i>"; 
               }
             
         });
